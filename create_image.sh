eab9b5bdc3a19661c46833b3a2d7edd5c9629ee8
AJ <bitjockey@jackpoint.network>
2021-11-14 13:18:50 -0600

    append gpu-jupyter to get the right image

--- create_image.sh
+++ create_image.sh
@@ -6,5 +6,6 @@ TF_VERSION=${1:-$DEFAULT}
 
 # Launch the container for building:
 echo "Launching container with tensorflow $TF_VERSION"
-tensorman +$TF_VERSION run --gpu --python3 --jupyter --root --name nlp_dev ./setup_container.sh
+tensorman pull $TF_VERSION-gpu-jupyter
+tensorman +$TF_VERSION run --gpu --jupyter --root --name nlp_dev ./setup_container.sh
 echo "You can now exit the container"
