4df4eabcae1e142bb7e174fb40a72f487211a4f5
AJ <bitjockey@jackpoint.network>
2021-11-14 12:55:10 -0600

    Move pip3 upgrade

--- setup_container.sh
+++ setup_container.sh
@@ -8,6 +8,11 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -y && apt-get install -y yarn
 
+# Update pip3
+echo "Updating pip3..."
+pip3 install --upgrade pip
+echo "Update complete."
+
 # Get CUDA version
 version_string=$(nvcc --version | grep "release" | awk '{print $6}' | cut -c2-)
 echo $version_string
@@ -15,11 +20,6 @@ major=$(cut -d'.' -f1 <<< $version_string)
 minor=$(cut -d'.' -f2 <<< $version_string)
 cuda_version="${major}${minor}"
 echo "${major}${minor}"
-
-# Update pip3
-echo "Updating pip3..."
-pip3 install --upgrade pip
-echo "Update complete."
 
 # Install required packages
 echo "Installing requirements..."
