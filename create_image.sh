b8c0cf3155e86b689e069d690be50f3dfc0ccadd
AJ <bitjockey@jackpoint.network>
2021-11-14 13:05:47 -0600

    Allow setup with different version of tensorflow

--- create_image.sh
+++ create_image.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 # This should be run on the host computer
+# Usage: ./create_image 2.1.0
+DEFAULT=2.1.0
+TF_VERSION=${1:-$DEFAULT}
 
-# Launch the container:
-echo "Launching container"
-tensorman run --gpu --python3 --jupyter --root --name nlp_dev ./setup_container.sh
+# Launch the container for building:
+echo "Launching container with tensorflow $TF_VERSION"
+tensorman +$TF_VERSION run --gpu --python3 --jupyter --root --name nlp_dev ./setup_container.sh
 echo "You can now exit the container"
